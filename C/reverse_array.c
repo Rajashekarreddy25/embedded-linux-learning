2383be704e8582217a33dabfea9ba83690795dbe
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-11 13:52:07 +0530

    Auto Commit - 2026-08-11 13:52:07

--- C/reverse_array.c
+++ C/reverse_array.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 int main(){
-	printf("hello ramalakshmi");
+	int n;
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements of the array: ");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("The reversed array is: ");
+	for(int i=n-1;i>=0;i--){
+		printf("%d ",arr[i]);
+	}
+	return 0;
 }
