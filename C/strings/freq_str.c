2940a3a550ffd872f2526019a2f2c6b01e714dfc
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-12 21:26:09 +0530

    Auto Commit - 2026-08-12 21:26:09

--- C/strings/freq_str.c
+++ C/strings/freq_str.c
@@ -8,6 +8,9 @@ int main(){
 	printf("enter a string:");
 	scanf("%s",str);
 	for(int i=0;str[i] != '\0';i++){
+		if (str[i] >= 65 && str[i] <=90){
+			res[str[i]-'A']++;
+		}
 		res[str[i]-'a']++;
 	}
 	for(int i=0;i<26;i++){
