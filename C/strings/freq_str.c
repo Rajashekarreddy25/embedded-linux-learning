fc63b2a21cdb67b90e011e2a4702225a6e86eb40
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-12 21:04:48 +0530

    Auto Commit - 2026-08-12 21:04:48

--- C/strings/freq_str.c
+++ C/strings/freq_str.c
@@ -6,7 +6,7 @@ int main(){
 	int res[256] = {0};
 
 	for(int i=0;str[i] != '\0';i++){
-		res[str[i]++];
+		res[str[i]]++;
 	}
 	for(int i=0;i<256;i++){
 		printf("%c is have repeated %d times:",i,res[i]);
