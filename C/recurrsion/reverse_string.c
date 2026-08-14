40f645ca29ca9fc4ea5842d6120d248b718f0d6a
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-14 22:37:43 +0530

    Auto Commit - 2026-08-14 22:37:43

---
+++ C/recurrsion/reverse_string.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+char reverse_str(char *str,int start,int end){
+	if (start == end )
+		return;
+	char temp = str[start];
+	str[start] = str[end];
+	str[end] = temp;
+	return reverse_str( &str,start+1,end-1);
+
+}
+int main(){
+	char str[] = "rajashekar";
+	int n = strlen(str)-1;
+	reverse_str(&str,0,n);
+	printf("after the reverse: %s\n",str);
+
+	return 0;
+}
+
+
