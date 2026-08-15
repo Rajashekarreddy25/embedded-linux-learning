2b9c84830d71de56b23852e47aa9c2e8b6438914
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-15 10:13:52 +0530

    Auto Commit - 2026-08-15 10:13:52

--- C/recurrsion/reverse_num.c
+++ C/recurrsion/reverse_num.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 
-void reverse_num(int num){
+void reverse_num(int num,int *rev){
 	if (num  <= 0)
 		return ;
 	int d = num%10;
-	int rev = 0;
 	rev *= 10 +d;
-	num /=10;
-	return 
+	return reverse_num(num/10,&rev);
+}
+int main(){
+	int num;
+	int rev=0;
+	printf("enter a number :");
+	scanf("%d",&num);
+	reverse_num(num,&rev);
+	printf("The number %d after the reverse : %d\n",num,rev);
 
+	return 0;
 }
 
 
