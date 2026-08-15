b0c2cc8abdc5499f66e0e9f3cf105fc6a99007ff
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-15 10:57:15 +0530

    Auto Commit - 2026-08-15 10:57:15

---
+++ C/recurrsion/sum_of_nums.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void sumOfDigits(int num,int *sum){
+	if (num == 0)
+		return ;
+	return sumOfDigits(num/10,sum + num/10);
+
+}
+int main(){
+	int num;
+	int sum = 0;
+	printf("enter a num:");
+	scanf("%d" , &num);
+
+	sumOfDigits(num,&sum);
+
+	printf("sum of digits :%d\n",sum);
+
+	return 0;
+}
