07c87918a66224587c40643b494c3ac1b848c86e
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-16 10:12:40 +0530

    Auto Commit - 2026-08-16 10:12:40

--- C/bit_manupulation/bit_fields/32_bit_color.c
+++ C/bit_manupulation/bit_fields/32_bit_color.c
@@ -7,7 +7,7 @@ int main(){
 	uint8_t green = 187;
 
 	uint32_t color = ((uint32_t)green) | ((uint32_t)blue<<8) | ((uint32_t)red<<16) ;
-	color |= ~(1<<24);
+//	color |= ~(1<<24);
 
 	printf("color = 0X%08X\n",color);
 	return 0;
