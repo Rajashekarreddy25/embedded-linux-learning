d44b736e527cbc27e6816c2740a614408de10dfa
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-18 21:29:50 +0530

    Auto Commit - 2026-08-18 21:29:50

--- C/linkedlist/ll_practice.c
+++ C/linkedlist/ll_practice.c
@@ -1,3 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+
+struct node{
+int data;
+struct node *next;
+};
+
+struct node *head = NULL;
+struct node *createNode(int data){
+	struct node *newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+
+void insert(struct node *head,int val){
+
+	struct node *temp = NULL;
+
+	if (head == NULL)
+		return ;
+	temp = head;
+	while(temp->next != NULL){
+		temp = temp->next;
+	}
+	temp->next = createnode(val);
+}
+
+void traverse(struct node *head){
+
+	struct node *temp = head;
+
+	while(temp != NULL){
+		printf("%d ->",temp->data);
+		temp = temp->next;
+	}
+
+}
+
+int main(){
+
+	struct node *head = NULL;
+
+	insert(&head,10);
+	insert(&head,20);
+	insert(&head,30);
+	insert(&head,40);
+	insert(&head,50);
+
+	traverse(&head);
+
+	return 0;
+}
