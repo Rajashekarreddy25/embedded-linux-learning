08326cb9614a213680724a74cec22e6a21a649a2
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-19 15:26:18 +0530

    Auto Commit - 2026-08-19 15:26:18

---
+++ C/arrays/sliding_window/subarray_sum.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+	int arr[] = {1,2,3,4,5,6};
+
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int k =3; //size of the subarray
+	
+	int sum = 0;
+
+	for (int i=0;i<k;i++){
+		sum+=arr[i];
+	}
+ //	printf("subarray values %d ",sum);
+	for(int j=k;j<size;j++){
+		int sumR = sum;
+		sum += arr[j];
+	        sum -= arr[j-k];
+       		if (sumR > sum)
+	 		sum = sumR;
+	}
+			printf("%d ",sum);
+	
+
+//	printf("The largest sum of subarray size %d is %d",k,sum);
+
+
+	return 0;
+}
+
