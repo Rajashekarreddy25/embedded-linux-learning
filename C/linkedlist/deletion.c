38855ab2f3a0d2640c167662ff64ac51f194da45
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-19 20:29:01 +0530

    Auto Commit - 2026-08-19 20:29:01

---
+++ C/linkedlist/deletion.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+struct node *createnode(struct node *head,int val){
+	struct node *newnode = (struct node*)malloc(struct node);
+
+	newnode->data = val;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+struct node *insertAtEnd(struct node *head,int val){
+	struct node *temp = head;
+
+	if(head == NULL){
+		return createnode(head,val);
+
+	while(temp->next != NULL){
+		temp = temp->next;
+
+	}
+	temp->next = createnode(head,val);
+
+	return head;
+}
+
+
+void Traverse(struct node *head){
+
+	struct node *temp = head;
+
+	while(temp != NULL){
+		printf("%d->",temp->data);
+		temp = temp->next;
+	}
+	printf("NULL\n");
+}
+
+void freeList(struct node *head){
+
+	struct node *temp = head;
+
+	while(head != NULL){
+
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+
+	printf("The list is successfully Freed\n");
+}
+
+
+
+int main(){
+
+	struct node *head = NULL;
+
+       	head = insertAtEnd(head,10);
+	 head = insertAtEnd(head,20);
+	 head = insertAtEnd(head,30);
+	 head = insertAtEnd(head,40);
+	 head = insertAtEnd(head,50);
+
+	 Traverse(head);
+
+	 freeList(head);
+
+	 return 0;
+}
+
+	
