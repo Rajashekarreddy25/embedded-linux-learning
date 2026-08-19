e08aebaf12941f1c8154b27806490b65cbb3dcba
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-19 22:32:16 +0530

    Auto Commit - 2026-08-19 22:32:16

---
+++ C/linkedlist/reverse_ll.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<unistd.h>
+
+struct node {
+	int data ;
+	struct node *next;
+};
+
+struct node *createNode(struct node *head,int val){
+
+	sturct node *newnode = (struct node*)malloc(sizeof(struct node));
+
+	newnode->data = val;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+struct node *insert(struct node *head,int val){
+
+	if(head == NULL)
+		return newnode;
+	struct node *temp = head;
+
+	while(temp->next != NULL){
+		temp = temp->next;
+	}
+
+	temp->next = createNode(head,val);
+
+	return head;
+}
+
+void Traverse(struct node *head){
+
+	struct node *temp = head;
+
+	while(temp != NULL){
+
+		printf("%d->",temp->data);
+		temp = temp->next;
+	}
+
+	printf("NULL\n");
+
+}
+
+void freeList(struct node *head){
+
+	struct node *temp = NULL;
+
+	while(head != NULL){
+
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	printf("The list is freed successfully");
+}
+
+
+
+int main(){
+
+	struct node *head = NULL;
+
+	head = insert(head,10);
+	head = insert(head,20);
+	head = insert(head,30);
+	head = insert(head,40);
+
+
+	Traverse(head);
+
+	freeList(head);
+
+	return 0;
+}
+
