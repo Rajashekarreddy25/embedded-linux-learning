f681996c14f347fd164e7be40887e5bd2a0e883e
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 13:16:13 +0530

    Auto Commit - 2026-08-23 13:16:13

--- C/arrays/freq_arr.c
+++ C/arrays/freq_arr.c
@@ -1,3 +1,25 @@
 #include<stdio.h>
 
-int 
+int main(){
+
+	int arr[] = {1,2,3,4,5,2,4,3,1,10};
+	
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+	int freq[256]={0};
+	
+	for(int i=0; i<n;i++){
+		freq[arr[i]++];
+	}
+
+	for(int i= 0 ;i<256;i++){
+		if (freq[i] == 0)
+			continue;
+		else
+			printf("The freq of %d is %d :",freq[arr[i]],freq[i]);
+
+	}
+
+	return 0;
+}
+		
