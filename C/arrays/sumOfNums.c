8761096f6b601671eebdf18e71a0eb37d4eae489
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 08:52:06 +0530

    Auto Commit - 2026-08-23 08:52:06

---
+++ C/arrays/sumOfNums.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int sumOfArray(int *arr){
+
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int sum=0;
+	for(int i=0;i<size;i++){
+		sum+=arr[i];
+	}
+	return sum;
+}
+
+int main(){
+	int n;
+	printf("enter the size of the arr:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter the arr elements");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("The sum of given arr ele is :%d",sumOfArray(arr));
+
+	return 0;
+}
+
+
