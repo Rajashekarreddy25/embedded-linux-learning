4f2da979a1505c34eb3af17f0e348b65f6f1809b
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 21:08:16 +0530

    Auto Commit - 2026-08-23 21:08:15

--- C/strings/strcpy.c
+++ C/strings/strcpy.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
 
 char strcmpr(char *str1,char *str2){
-	int i=0;
-	int j=0;
-	while(str1[i] != '\0' && str2[j] != '\0'){
+	
+	while( *str1 && (*str1 == *str2) ){
 
-		if (str1[i] != str2[j]){
-
-			return str1[i] - str2[j];
-		}
-		i++;
-		j++;
+		*str1++;
+		*str2++;
 	}	
-	return 0;
+	return *str1 - *str2;
 	}
 
 
@@ -21,7 +16,7 @@ int main(){
 	char str1[] = "rajashekar";
 	char str2[] = "rajashekar";
 
-	int res = strcmpr(str1,str2);
+	int res = strcmpr(&str1,&str2);
 
 	if (res == 0)
 		printf("both strings are equal");
