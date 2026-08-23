ba8baf1dab8a7d760e159c70c8a6f15b9206561f
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 20:01:04 +0530

    Auto Commit - 2026-08-23 20:01:04

--- C/strings/str_panlindrome.c
+++ C/strings/str_panlindrome.c
@@ -4,23 +4,26 @@
 
 int main(){
 
-	char str[] = "maddiam";
-
+	char str[] = "maddam";
+	int ispalindrome = 1;
 
 	int start = 0;
 	int end = strlen(str)-1;
 
 	while(start < end){
 		if (str[start] != str[end] ){
-			printf("Not Palindrome");
+			//printf("Not Palindrome");
+			ispalindrome = 0;
 			break;
 		}
 
 		start++;
 		end--;
 	}
-
-	printf("Palindrome");
+	if(ispalindrome)
+		printf("Palindrome");
+	else
+		printf("Not Palindrome");
 
 return 0;
 
