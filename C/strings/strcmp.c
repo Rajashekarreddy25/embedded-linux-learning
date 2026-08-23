46dd6aa3e5ba97acd424c5cd94da6819dffe4362
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 21:48:32 +0530

    Auto Commit - 2026-08-23 21:48:32

---
+++ C/strings/strcmp.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+char strcmpr(char *str1,char *str2){
+	
+	while( *str1 && (*str1 == *str2) ){
+
+		str1++;
+		str2++;
+	}	
+	return *str1 - *str2;
+	}
+
+
+int main(){
+
+	char str1[] = "rajashekar";
+	char str2[] = "rajahekar";
+
+	int res = strcmpr(str1,str2);
+
+	if (res == 0)
+		printf("both strings are equal");
+	else
+		printf("both are not equal");
+
+	return 0;
+}
