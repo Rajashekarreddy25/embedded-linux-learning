5bd68794d901ef5bff8426eb4d37369f3cf98bda
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 19:36:37 +0530

    Auto Commit - 2026-08-23 19:36:37

---
+++ C/strings/non_repeatingChar.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+
+char nonRepeatingChar(char *str){
+
+	int n = strlen(str)-1;
+	for(int i=0;i<n;i++){
+		int found = 0;
+		for(int j=i+1;j<n;j++){
+			
+			if( i!=j && str[i] == str[j]){
+				found = 1;
+				break;
+			}
+		}
+		if(!found)
+		//	printf("The first non repeating char is :%c",str[i]);
+			return str[i];
+
+	return '$';
+}
+
+int main(){
+
+	char str[] = "rajashekarreddy";
+
+	printf("The first non repeating char is :%c ",nonRepeatingChar(str);
+
+	return 0;
+}
+
+
