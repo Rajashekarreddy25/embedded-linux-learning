a422009a0ad3feb62c7d4efbd73102015df98167
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-23 18:36:48 +0530

    Auto Commit - 2026-08-23 18:36:48

--- C/arrays/separate_evenOdd.c
+++ C/arrays/separate_evenOdd.c
@@ -5,7 +5,7 @@ int main(){
 
 	int n = sizeof(arr)/sizeof(arr[0]);
 
-	for(int i = 0;i<n;i++){
+/*	for(int i = 0;i<n;i++){
 		if (arr[i] %2 == 0){
 			int j = i+1;
 			while(j<n && arr[j] %2 == 0){
@@ -22,7 +22,25 @@ int main(){
 
 	}	
 
+*/
 
+	int start = 0;
+	int end = n-1;
+	while(start<end){
+
+		while(start<end && arr[start] != 0)
+			start++;
+		while(start < end && arr[end] == 0)
+			end--;
+		if(start < end){
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+
+			start++;
+			end--;
+		}
+	}
 
 	for(int i = 0;i<n;i++){
 		printf("%d ",arr[i]);
