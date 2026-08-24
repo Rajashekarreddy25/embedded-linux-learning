3279413164e2deb836ad43b06e877eaab9faf809
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-24 09:14:36 +0530

    Auto Commit - 2026-08-24 09:14:36

---
+++ C/bit_manupulation/reverse32BitReg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdint.h>
+
+int main(){
+
+	uint32_t reg = 5;
+	
+	uint32_t  res =0;
+
+	printf("%u ",reg);
+
+	for (int i=0;i<32;i++){
+		uint32_t bit = reg&1;
+		res = (res<<1)|bit;
+		reg = reg>>1;
+	}
+	printf("the no after reverse is :%u ",res);
+
+	return 0;
+}
+
