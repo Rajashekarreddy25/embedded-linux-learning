f2d43dfc1302f2a0a23b833d2eb09dab9522eeab
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-24 14:39:42 +0530

    Auto Commit - 2026-08-24 14:39:42

---
+++ C/pointers/reverseArr.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int reverse(int *arr,int size){
+	
+	int i=0;
+	int *endp = arr;
+       	endp = end+size;	
+/*
+	while(i<size){
+		i++;
+		endp++;
+	}
+	*/
+	while(i<size/2){
+		i++;
+		int temp = *arr;
+		*arr = *endp;
+		*endp = *arr;
+		arr++;
+		endp--;
+	}
+
+
+int main(){
+
+	int arr[]= {1,2,3,4,5};
+	
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+	reverse(&arr,n);
+	
+	for(int i=0;i<n;i++){
+
+		printf("%d ",arr[i]);
+	}
+
+	return 0;
+}
+
+
+
