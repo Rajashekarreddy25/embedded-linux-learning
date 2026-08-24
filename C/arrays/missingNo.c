4ea70d71e7f5d2688d0a605ed0e1db52e87f0a29
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-24 09:24:27 +0530

    Auto Commit - 2026-08-24 09:24:27

--- C/arrays/missingNo.c
+++ C/arrays/missingNo.c
@@ -7,12 +7,13 @@ int main(){
 	int n = sizeof(arr)/sizeof(arr[0]);
 
 	int sum = (n*(n+1))/2;
+	printf("sum of n nums is %d\n",sum);
 
 	int arrSum = 0;
 	for (int i = 0;i<n;i++){
 		arrSum += arr[i];
 	}
-
+	 printf("ARRAY 	sum of n nums is %d\n",arrSum);
 	int miss = sum - arrSum;
       printf("The missing no is :%d",miss);
 
