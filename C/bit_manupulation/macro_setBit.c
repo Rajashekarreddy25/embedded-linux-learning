a5f3e9cf4ed51899fcc83ccd3d5be0fee6920135
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-25 17:10:30 +0530

    Auto Commit - 2026-08-25 17:10:30

---
+++ C/bit_manupulation/macro_setBit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#define SET(n,pos) (n&(1>>pos))
+
+int main(){
+
+	int n;
+	printf("enter a value");
+	scanf("%d",&n);
+
+	if (SET(n,3))
+		printf("set");
+	else
+		printf("NOT set");
+
+	return 0;
+}
