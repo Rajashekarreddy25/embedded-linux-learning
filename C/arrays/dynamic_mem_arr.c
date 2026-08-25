8a861cc5d9be6a8f810fbda7753df46f706fead9
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-25 15:45:25 +0530

    Auto Commit - 2026-08-25 15:45:25

--- C/arrays/dynamic_mem_arr.c
+++ C/arrays/dynamic_mem_arr.c
@@ -2,10 +2,14 @@
 #include<stdlib.h>
 
 int main(){
+	
+	int n ;
+	printf("enter the size of the arr:");
+	scanf("%d",&n);
 
-	int arr ;
 	int *ptr= (int*)malloc(10*sizeof(int));
-
+	
+	printf("enter the %d values into the arr",n);
 	for (int i=0;i<10;i++){
 
 		scanf("%d",(ptr+i));
