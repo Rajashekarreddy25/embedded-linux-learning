cd4cf330dd7069f77776714335c30ffb0a2bd917
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-26 09:03:15 +0530

    Auto Commit - 2026-08-26 09:03:15

---
+++ C/linkedlist/findMiddleNode.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+	int data ;
+	struct node *next;
+};
+
+struct node *createNode(struct node *head,int val){
+
+	struct node *newnode = (struct node*)malloc(sizeof(struct node));
+
+	newnode->data = val;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+struct node *insertAtEnd(struct node *head,int val){
+
+	struct node *temp = head;
+
+	if (head == NULL)
+		return createNode(head,val);
+
+	while(temp->next != NULL){
+		temp= temp->next;
+	}
+
+	temp->next = createNode(head,val);
+
+	return  head;
+}
+
+void Traverse(struct node *head){
+
+	struct node *temp = head;
+
+	while(temp != NULL){
+		printf("%d->",temp->data);
+		temp = temp->next;
+	}
+
+	printf("NULL\n");
+}
+
+int main(){
+
+	struct node *head = NULL;
+
+	head = insertAtEnd(head,10);
+
+	head = insertAtEnd(head,20);
+
+	head = insertAtEnd(head,30);
+
+	head = insertAtEnd(head,40);
+
+	head = insertAtEnd(head,50);
+
+	Traverse(head);
+
+
+	return 0;
+}
+	
+
+
