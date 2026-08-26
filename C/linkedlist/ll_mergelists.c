7fcc2b8ad1a842159442a115f3ab169eb78dcb29
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-26 10:29:14 +0530

    Auto Commit - 2026-08-26 10:29:14

--- C/linkedlist/ll_mergelists.c
+++ C/linkedlist/ll_mergelists.c
@@ -19,18 +19,57 @@ struct node *create(struct node *head,int val){
 
 struct node *insert(struct node *head,int val){
 
-//	struct node *temp = head;
-	
-//	struct node *newnode = create(head,val);
 	if(head == NULL)
 		return create(head,val);
 
-	 struct node *newnode = create(head,val);
+	struct node *newnode = create(head,val);
+
 	newnode->next = head;
        	head = newnode;
 
 	return head;
 }
+
+struct node *merge(struct node *head1,struct node *head2,struct node *head3){
+
+	struct node *temp1 = head1;
+
+	struct node *temp2 = head2;
+
+	struct node *temp3 = head3;
+
+	while( temp1 != NULL && temp2 != NULL){
+
+		if(temp1->data < temp2->data){
+
+			temp3->data = temp1->data;
+			temp1 = temp->next;
+
+		}
+		else{
+			temp3->data = temp2->data;
+			temp2 = temp2->next;
+		}
+
+		while(temp1 != NULL){
+		      
+		       	temp3->data = temp1->data;
+			
+			temp3 = temp3->next;
+		}
+
+		while(temp2 != NULL){
+			
+			temp3 ->data = temp2->data;
+			
+			temp3 = temp3->next;
+		}
+	}
+}	
+
+
+
+
 
 void Traverse (struct node *head){
 
