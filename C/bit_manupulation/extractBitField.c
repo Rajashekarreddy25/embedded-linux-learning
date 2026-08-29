927ca963da8aa02da541b0d0751c25eb0c04fc5e
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-29 14:29:05 +0530

    Auto Commit - 2026-08-29 14:29:05

---
+++ C/bit_manupulation/extractBitField.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#include<stdint.h>
+
+
+int main(){
+
+	uint32_t n;
+	printf("enter a no:");
+	scanf("%uu",&n);
+	printf("entered no in binary is : %b\n",n);
+
+	return 0;
+}
+
+
+
