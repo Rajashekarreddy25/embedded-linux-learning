0d27f7810d7fd2a0610cd9637297ed2502f0a18f
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-29 14:34:46 +0530

    Auto Commit - 2026-08-29 14:34:46

--- C/bit_manupulation/extractBitField.c
+++ C/bit_manupulation/extractBitField.c
@@ -9,6 +9,11 @@ int main(){
 	printf("enter a no:");
 	scanf("%u",&n);
 	printf("entered no in binary is : %b\n",n);
+	
+	// To extract the n bit from postion pos 
+	uint32_t val = (n>>pos) & 0xf;
+
+	printf("%b\n",val);
 
 	return 0;
 }
