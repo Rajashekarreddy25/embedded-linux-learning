076b82fd0c9e64cb63704c6efb6931fcafecbfca
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-29 10:07:02 +0530

    Auto Commit - 2026-08-29 10:07:02

--- C/2Darrays/reverseRows.c
+++ C/2Darrays/reverseRows.c
@@ -2,11 +2,25 @@
 
 int main(){
 
+
+	int n ;
+	printf("enter the size of square matrix");
+	scanf("%d",&n);
+
+	int arr[n][n] ;
+	printf("enter the ele in the matrix:");
+	for (int i = 0;i<n;i++){
+		for (int j = 0;j<n;j++){
+
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	/*
 	int arr[3][3] = {{1,2,3},
 			{4,5,6},
 			{7,8,9}};
 
-
+	*/
 	for (int i = 0;i<3;i++){
 
 		for(int j =0;j<3;j++){
