a4c3ea4e0a2064ae8d181097c4fe3ba22b438c02
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-30 17:19:40 +0530

    Auto Commit - 2026-08-30 17:19:40

--- C/arrays/sliding_window/subarray_sum.c
+++ C/arrays/sliding_window/subarray_sum.c
@@ -6,17 +6,17 @@ int main(){
 	int size = sizeof(arr)/sizeof(arr[0]);
 	int k =3; //size of the subarray
 	
-	int sum = 0;
+	int window_sum = 0;
 
 	for (int i=0;i<k;i++){
-		sum+=arr[i];
+		window_sum += arr[i];
 	}
- 	printf("sum of subarray values of size :%d is  %d ",k,sum);
+ 	printf("sum of subarray values of size :%d is  %d ",k,window_sum);
 	for(int j=k;j<size;j++){
-		sum += arr[j];
-	        sum -= arr[j-k];
+		window_sum += arr[j];
+	        window_sum -= arr[j-k];
 	
-			printf("%d ",sum);
+			printf("%d ",window_sum);
 	
 	}
 
