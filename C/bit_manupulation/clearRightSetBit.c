3dba85b7c5d2e4630c02d00da1db949b077935d7
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-30 16:26:12 +0530

    Auto Commit - 2026-08-30 16:26:12

--- C/bit_manupulation/clearRightSetBit.c
+++ C/bit_manupulation/clearRightSetBit.c
@@ -3,9 +3,9 @@
 int main(){
 
 	int n;
-	printf("enter a number :");
+	printf("enter a number :\n");
 	scanf("%d",&n);
-	printf("the given numbwer is : %b");
+	printf("the given numbwer is : %b\n",n);
 	n = n &(n-1);
 
 	printf("The no after clearing last set bit: %b",n);
