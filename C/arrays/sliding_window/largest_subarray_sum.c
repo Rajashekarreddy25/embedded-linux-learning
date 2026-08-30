98328f95578a3f69cfad019c67525ce7e456bf2d
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-30 17:16:48 +0530

    Auto Commit - 2026-08-30 17:16:48

--- C/arrays/sliding_window/largest_subarray_sum.c
+++ C/arrays/sliding_window/largest_subarray_sum.c
@@ -6,25 +6,23 @@ int main(){
 	int size = sizeof(arr)/sizeof(arr[0]);
 	int k =3; //size of the subarray
 	
-	int sum = 0;
+	int window_sum = 0;
 
 	for (int i=0;i<k;i++){
-		sum+=arr[i];
+		window_sum += arr[i];
 	}
- //	printf("subarray values %d ",sum);
-	for(int j=k;j<size;j++){
-		int sumR = sum;
-		sum += arr[j];
-	        sum -= arr[j-k];
-       		if (sumR > sum)
-	 		sum = sumR;
-	}
-			printf("%d ",sum);
+
+	int max_sum = window_sum;
 	
+	for(int j=k;j<size;j++){
+	
+		window_sum += arr[j];
+	        window_sum -= arr[j-k];
+       		if (window_sum > max_sum)
+	 		max_sum = window_sum;
+	}
 
-//	printf("The largest sum of subarray size %d is %d",k,sum);
-
-
+	printf("%d ",max_sum);
 	return 0;
 }
 
