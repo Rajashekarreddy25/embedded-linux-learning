adefb782b929ccfc05df4c50bbc56477cb436449
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-31 11:24:53 +0530

    Auto Commit - 2026-08-31 11:24:53

---
+++ C/bit_manupulation/bit_fields/rotate_bits_left.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#include<stdint.h>
+
+uint32_t reverse_left(uint32_t reg,int k){
+
+	return (reg<<k) | (reg>>(32-k));
+}
+
+int main(){
+
+	uint32_t reg;
+	int k;
+	printf("enter a reg value and k value");
+
+	scanf("%uu %d",&reg,&k);
+
+	printf("The binary of reg is:\n %b",reg);
+
+	printf("\nThe revesed binary is %b",reverse_left(reg,k));
+
+	return 0;
+}
