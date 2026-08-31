d6a3acf5da602921a22366e70a9fca282a7aacee
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-08-31 12:45:34 +0530

    Auto Commit - 2026-08-31 12:45:34

--- C/basic_programs/practice.c
+++ C/basic_programs/practice.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
 int main(){
+	/*
 	int a[] = {1,2,3,4,5};
 	int *p = &a;
 	printf("%d",p);
+*/
+	char str[] = "Hello";
+	char *ptr = str;
+	printf("%d", sizeof(ptr));
+	printf("%d", strlen(ptr));
+
 	return 0;
 }
 
