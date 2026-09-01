06c0028c168eecd91ab433d19d2df475269d369a
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-09-01 15:25:14 +0530

    Auto Commit - 2026-09-01 15:25:14

---
+++ C/2Darrays/spiral_matrix.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main(){
+
+	int arr[4][4] = {{1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12},
+			{13,14,15,16}};
+
+	int n = 4;
+
+	int top = 0;
+	int left = 0;
+	int bottom = n-1;
+	int right = n-1;
+
+	while((top <= bottom) && (left <= right)) {
+		
+		for(int col = 0;col<n;col++){
+
+			printf("%d ",arr[top][col]);
+
+		}
+		top++;
+		for(int row = top; row < n;row++){
+
+			printf("%d ",arr[row][right]);
+		}
+
+		right--;
+
+		for (int col = right;col >= left; col--){
+
+			printf("%d " ,arr[bottom][col]);
+		}
+
+		bottom--;
+		
+		for(int row = bottom ;row >= top; row++){
+
+			printf("%d ",arr[row][left]);
+		}
+		left++;
+	}
+
+return 0;
+}
+
+
