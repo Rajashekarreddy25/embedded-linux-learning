2488dc75cb6c7a082104118cc0b8bc0776f7da35
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-09-03 12:09:15 +0530

    Auto Commit - 2026-09-03 12:09:15

--- C/bit_manupulation/unsigned_to_positive.c
+++ C/bit_manupulation/unsigned_to_positive.c
@@ -2,10 +2,11 @@
 #include<stdint.h>
 int main(){
 
-	int x  = 6;
-		
-	x = (~x) + 1;
+	int32_t x  = 6;
+	int32_t mask = 1<<31;	
+//	x = (~x) + 1;
 //	x &= ~(1U << 31);
+	x = (x^mask) - mask;
 	printf("%d ",x);
 
 	return 0;
