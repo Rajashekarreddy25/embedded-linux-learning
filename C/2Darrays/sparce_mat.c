16ddf208c536dbc97afe558d1a417045008bb818
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-09-04 14:46:01 +0530

    Auto Commit - 2026-09-04 14:46:01

---
+++ C/2Darrays/sparce_mat.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+	int mat[5][5]= {{0,0,3,0,4},
+		      {0,0,5,7,0},
+		      {0,0,0,0,0},
+		      {0,3,2,0,0},
+		      {0,2,6,0,0}};
+
+//	int res[][3] ;
+	printf(" Row   coloum value\n ");
+	for (int i =0 ;i<5;i++){
+		for(int j = 0;j<5;j++){
+			
+			if(mat[i][j] != 0){
+
+				printf(" %d   %d  %d ",i,j,mat[i][j]);
+			}
+
+
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
+
