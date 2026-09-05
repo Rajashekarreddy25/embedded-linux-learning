1d61682ff1e4b9953b580b1e8b9be58676fc91e6
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-09-05 11:58:49 +0530

    Auto Commit - 2026-09-05 11:58:49

--- C/arrays/firstStableIndex.c
+++ C/arrays/firstStableIndex.c
@@ -5,7 +5,7 @@ int main(){
 int nums[] = {5,0,1,4};
 int n = sizeof(nums)/sizeof(nums[0]);
 
-int  k = 3;
+int  z = 3;
 int res = -1;
 
     for(int i = 0;i<n;i++){
@@ -23,7 +23,7 @@ int res = -1;
             }
         }
 
-        if ((maxval - minval) <= k){
+        if ((maxval - minval) <= z){
 
             res = i;
 	    break;
